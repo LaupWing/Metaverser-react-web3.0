4b402221602fb0e691f45c0fa720924567e534d1
LaupWing <loc_strict@hotmail.com>
2022-01-11 10:13:44 +0100

    UPdate

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,23 @@
+import { useMoralis } from "react-moralis"
+
 const Login = () => {
+   const {authenticate} = useMoralis()
+
    return (
       <div>
          Login screen
+         <div className='flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4'>
+            <Image 
+               src="https://links.papareact.com/3pi"
+               width={200}
+               height={200}
+               className='object-cover rounded-full'
+            />
+            <button onClick={authenticate} className='bg-yellow-500 rounded-lg p-5 font-bold animate-pulse'>Login to the METAVERSE</button>
+         </div>
+         <div className='w-full h-screen'>
+            <Image src="https://links.papareact.com/55n" layout='fill' objectFit='cover'/>
+         </div>
       </div>
    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useMoralis } from 'react-moralis'
 import Login from '../components/Login'
 
 export default function Home() {
-   const isAuthenticated = true
+   const {isAuthenticated} = useMoralis()
 
    if(!isAuthenticated) return <Login/>
 
@@ -13,18 +14,7 @@ export default function Home() {
             <title>Metaverse challenge</title>
             <link rel="icon" href="/favicon.ico" />
          </Head>
-         <div className='flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4'>
-            <Image 
-               src="https://links.papareact.com/3pi"
-               width={200}
-               height={200}
-               className='object-cover rounded-full'
-            />
-            <button className='bg-yellow-500 rounded-lg p-5 font-bold animate-pulse'>Login to the METAVERSE</button>
-         </div>
-         <div className='w-full h-screen'>
-            <Image src="https://links.papareact.com/55n" layout='fill' objectFit='cover'/>
-         </div>
+         
       </div>
    )
 }
