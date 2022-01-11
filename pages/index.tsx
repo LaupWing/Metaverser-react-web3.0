c0d2c8f030ae76f57611de8343c7670df9ec2f18
LaupWing <loc_strict@hotmail.com>
2022-01-11 09:51:04 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,14 @@ export default function Home() {
             <title>Metaverse challenge</title>
             <link rel="icon" href="/favicon.ico" />
          </Head>
-         <div>
-
+         <div className='flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4'>
+            <Image 
+               src="https://links.papareact.com/3pi"
+               width={200}
+               height={200}
+               className='object-cover rounded-full'
+            />
+            <button className='bg-yellow-500 rounded-lg p-5 font-bold animate-pulse'>Login to the METAVERSE</button>
          </div>
          <div className='w-full h-screen'>
             <Image src="https://links.papareact.com/55n" layout='fill' objectFit='cover'/>
